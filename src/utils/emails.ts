0b1e7c49a8766c95016396431031281ddaa886ee
Jovert Palonpon <jovertical@Joverts-MacBook-Air.local>
2023-04-06 01:44:53 +0800

    fix: mail sender ssl error

--- src/utils/emails.ts
+++ src/utils/emails.ts
@@ -6,19 +6,16 @@ type EmailPayload = {
   html: string;
 };
 
-const smtpOptions = {
-  host: process.env.NEXT_MAIL_HOST || '0.0.0.0',
-  port: parseInt(process.env.NEXT_MAIL_PORT || '1025'),
-  secure: process.env.NEXT_MAIL_ENCRYPTION === 'tls',
-  auth: {
-    user: process.env.NEXT_MAIL_USERNAME,
-    pass: process.env.NEXT_MAIL_PASSWORD,
-  },
-};
-
 export const sendEmail = async (data: EmailPayload) => {
   const transporter = nodemailer.createTransport({
-    ...smtpOptions,
+    host: process.env.NEXT_MAIL_HOST || '0.0.0.0',
+    port: parseInt(process.env.NEXT_MAIL_PORT || '1025'),
+    secure: false,
+    requireTLS: process.env.NEXT_MAIL_ENCRYPTION === 'tls',
+    auth: {
+      user: process.env.NEXT_MAIL_USERNAME,
+      pass: process.env.NEXT_MAIL_PASSWORD,
+    },
   });
 
   const info = await transporter.sendMail({
