48c3798d2134fea406b5d79e680c4f9c71af49cf
Jovert Palonpon <jovertical@Joverts-MacBook-Air.local>
2023-04-07 12:04:35 +0800

    replace feed util with rss package

--- src/app/rss.xml/route.ts
+++ src/app/rss.xml/route.ts
@@ -1,4 +1,5 @@
-import * as feed from '@/utils/feed';
+import Rss from 'rss';
+
 import { getArticles } from '@/data/article.data';
 import { rescue } from '@/utils';
 
@@ -6,7 +7,26 @@ export async function GET() {
   const feedXml = await rescue(async () => {
     const articles = await getArticles();
 
-    return feed.generate(articles);
+    // prettier-ignore
+    const feed = new Rss({
+      title: 'Articles - Jovert Palonpon',
+      description: 'All of my thoughts on programming, web and mobile app development, dev ops, and more.',
+      feed_url: 'https://www.jovertical.dev/rss.xml',
+      site_url: 'https://www.jovertical.dev',
+      language: 'en',
+    });
+
+    articles.forEach((article) => {
+      feed.item({
+        title: article.title,
+        description: article.excerpt,
+        url: `https://www.jovertical.dev/articles/${article.slug}`,
+        guid: `https://www.jovertical.dev/articles/${article.id}`,
+        date: article._publishedAt,
+      });
+    });
+
+    return feed.xml();
   });
 
   return new Response(feedXml, {
