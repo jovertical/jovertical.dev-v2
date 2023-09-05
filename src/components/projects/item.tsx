1d44aca02ba59bc28cf45043c8d1a6e78867ff95
Jovert Palonpon <jovertical@gmail.com>
2023-09-05 17:07:36 +0800

    feat: default project logo

--- src/components/projects/item.tsx
+++ src/components/projects/item.tsx
@@ -1,9 +1,10 @@
+import { LinkIcon } from '@heroicons/react/24/solid';
 import cx from 'classnames';
+import Image from 'next/image';
 import type { ComponentPropsWithoutRef } from 'react';
 
 import type { Project } from '@/data/project.data';
-import { LinkIcon } from '@heroicons/react/24/solid';
-import Image from 'next/image';
+import GithubIcon from '@/components/icons/github-icon';
 
 interface Props extends ComponentPropsWithoutRef<'article'> {
   data: Project;
@@ -15,17 +16,19 @@ export default function Item({ className, data: project, ...props }: Props) {
       {...props}
       className={cx('group relative flex flex-col items-start', className)}
     >
-      {project.logo && (
-        <div className="relative z-10 flex items-center justify-center w-12 h-12 bg-white rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+      <div className="relative z-10 flex items-center justify-center w-12 h-12 bg-white rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+        {project.logo ? (
           <Image
-            className="w-8 h-8 rounded-full"
+            className="w-8 h-8 rounded-full object-cover object-center"
             src={project.logo.url}
             alt={`${project.name} Logo`}
             width={project.logo.width}
             height={project.logo.height}
           ></Image>
-        </div>
-      )}
+        ) : (
+          <GithubIcon className="w-8 h-8 fill-zinc-800 dark:fill-zinc-200"></GithubIcon>
+        )}
+      </div>
 
       <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
         <div className="absolute z-0 transition scale-95 opacity-0 -inset-x-4 -inset-y-6 bg-zinc-50 group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"></div>
